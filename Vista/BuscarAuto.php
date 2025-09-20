186804967baf9b80d782b2b09076aa7b2daaa691
Alejo Lopez <LopezAlejo0124@gmail.com>
2025-09-20 10:22:36 -0300

    Estilo 'BuscarAuto.php'

--- Vista/BuscarAuto.php
+++ Vista/BuscarAuto.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <?php include_once __DIR__ . "/../includes/head.php" ?>
     <title>Buscar Auto</title>
 </head>
 <body>
-    <h1>Busqueda de Auto</h1>
-    <form action="Acciones/AccionBuscarAuto.php" method="post">
-        <label for="patente"></label>
-        <input type="text" id="patente" name="patente">
-        <input type="submit">
-    </form>
+    <div class="container my-4">
+        <h1 class="mb-4 text-center">Búsqueda de Auto</h1>
+        <form action="Acciones/AccionBuscarAuto.php" method="post" class="needs-validation" novalidate>
+            <div class="mb-3">
+                <label for="patente" class="form-label">Patente</label>
+                <input type="text" id="patente" name="patente" class="form-control" placeholder="Ingrese la patente" required>
+                <div class="invalid-feedback">
+                    Por favor ingrese una patente válida
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Buscar</button>
+        </form>
+    </div>
 </body>
 </html>
